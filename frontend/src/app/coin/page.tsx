b4fddf6fbdc7e8acd58f9f4e96d8eb44cd6b6ab6
wasabijiro <shushu011023@gmail.com>
2023-10-25 05:05:32 +0900

    add star

--- frontend/src/app/coin/page.tsx
+++ frontend/src/app/coin/page.tsx
@@ -15,22 +15,13 @@ export default function Coin() {
     r3: "0x" + colors.r3.toString(16),
   };
 
-  console.log(hexColors);
-  console.log(hexColors.l2);
-  console.log(Number(hexColors.l2));
   const props = {
-    l1: 16765404, // parseInt(hexColors.l1, 16),
-    l2: parseInt(hexColors.l2, 16), //17976100, //Number(hexColors.l2),
+    l1: parseInt(hexColors.l1, 16),
+    l2: parseInt(hexColors.l2, 16),
     l3: parseInt(hexColors.l3, 16),
     r1: parseInt(hexColors.r1, 16),
     r2: parseInt(hexColors.r2, 16),
     r3: parseInt(hexColors.r3, 16),
-    // l1: 0xffd1dc,
-    // l2: 0xaec6cf,
-    // l3: 0xb39eb5,
-    // r1: 0xbfd3c1,
-    // r2: 0xfff5b2,
-    // r3: 0xffb347,
     date: "2023/10/20",
     num: "#000",
   };
