d0c79aee454adef41d2ce739607e1c3e2b52776d
wasabijiro <shushu011023@gmail.com>
2023-11-01 09:46:21 +0900

    rm my-app

--- my-app/src/libs/moveCall/coco/init.ts
+++
@@ -1,9 +0,0 @@
-import * as item from "./item/structs";
-import * as myNft from "./my-nft/structs";
-import {StructClassLoader} from "../_framework/loader";
-
-export function registerClasses(loader: StructClassLoader) { loader.register(item.CoCoItem);
-loader.register(item.ITEM);
-loader.register(myNft.CoCoNFT);
-loader.register(myNft.MY_NFT);
- }
