93f75513e36bba133e3e495d6caa47c043c4914d
imataka7 <t.imagawa@kakehashi.life>
2023-11-03 11:47:15 +0900

    rm my-app and scripts

--- my-app/src/libs/moveCall/sui/types/functions.ts
+++
@@ -1,5 +0,0 @@
-import {PUBLISHED_AT} from "..";
-import {GenericArg, Type, generic} from "../../_framework/util";
-import {TransactionBlock} from "@mysten/sui.js/transactions";
-
-export function isOneTimeWitness( txb: TransactionBlock, typeArg: Type, t: GenericArg ) { return txb.moveCall({ target: `${PUBLISHED_AT}::types::is_one_time_witness`, typeArguments: [typeArg], arguments: [ generic(txb, `${typeArg}`, t) ], }) }
